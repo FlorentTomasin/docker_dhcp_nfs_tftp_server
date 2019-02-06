3a44206a64d682bfcf7fea62623d366bd8e028d4
Florent Tomasin <florenttomasin@orange.fr>
2019-02-06 10:57:28 +0000

    Test to be re-write.
    Signed-off-by: Florent Tomasin <florenttomasin@orange.fr>

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-# NFS config
-echo "NFS init..."
-/usr/sbin/exportfs -rv
-/sbin/rpcbind -s
-/usr/sbin/rpc.nfsd --no-nfs-version 2 --no-nfs-version 3 8 |:
-/usr/sbin/rpc.mountd
 # DHCP config
 echo "DHCP init..."
 /usr/sbin/dhcpd -4 --no-pid -cf /etc/dhcp/dhcpd.conf -lf /var/lib/dhcp/dhcpd.leases
 # TFTP config
 echo "TFTP init..."
-in.tftpd -L -c -s /var/tftpboot
+in.tftpd -c -s /var/tftpboot
+# NFS config
+echo "NFS init..."
+/sbin/rpcbind -s
+/sbin/rpcinfo
+/usr/sbin/rpc.nfsd -d --no-nfs-version 2 --no-nfs-version 3 8 |:
+/usr/sbin/exportfs -rv
+/usr/sbin/rpc.mountd --no-nfs-version 2 --no-nfs-version 3
+
+while true; do
+
+  pid=`pidof rpc.mountd`
+  
+  if [ -z "$pid" ]; then
+    echo "NFS has failed, exiting, so Docker can restart the container..."
+    break
+  fi
+
+  sleep 1
+
+done
